b69e66a251641bb8b256b368dae94dbfbe5a07b0
divinevr777 <147039756+divinevr777@users.noreply.github.com>
2023-10-13 11:04:07 +0530

    Add files via upload

---
+++ alpbh.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  int alphabet = 65;
+
+  for (int i = 0; i <= 8; i++) {
+    for (int j = 0; j <= i; j++) {
+      stdout.write('${String.fromCharCode(alphabet + j)} ');
+    }
+    stdout.writeln();
+  }
+}
